d217de36b1fde2b41617668fb325008b17fa39be
Aditya <adiadi7898@gmail.com>
2022-05-11 15:45:26 +0530

    Fixed duplicate news issue and added a sticky navbar

--- src/component/News.js
+++ src/component/News.js
@@ -8,16 +8,16 @@ export default class News extends Component {
 
   static defaultProps = {
     pageSize: 6,
-    category:"General"
+    category: "General"
   }
 
   static propTypes = {
     pageSize: PropTypes.number,
     category: PropTypes.string
-    
+
   }
 
-  
+
 
   constructor() {
     super();
@@ -25,48 +25,47 @@ export default class News extends Component {
     this.state = {
       articles: [],
       loading: false,
-      page:1,
+      page: 1,
       totalResults: 0
     }
   }
 
-  async updatePage(){
+  async updatePage() {
     let url = `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=cda03860184d4e7989712e1e28ba1d8c&page=${this.state.page}pageSize=${this.props.pageSize}`;
-    this.setState({loading: true})
+    this.setState({ loading: true })
     let data = await fetch(url);
     let parseddata = await data.json();
     console.log(parseddata);
     this.setState({
       articles: parseddata.articles,
       loading: false
-     })
+    })
 
   }
 
   async componentDidMount() {
     const url = `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=cda03860184d4e7989712e1e28ba1d8c&&page=1&pageSize=${this.props.pageSize}`;
-    this.setState({loading: true})
+    this.setState({ loading: true })
     let data = await fetch(url);
     let parseddata = await data.json();
     console.log(parseddata);
     this.setState({
       articles: parseddata.articles,
       loading: false
-     })
+    })
   }
 
 
-  fetchMoreData=async ()=>{
-    this.setState({page: this.state.page + 1})
-    let url = `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=cda03860184d4e7989712e1e28ba1d8c&page=${this.state.page}pageSize=${this.props.pageSize}`;
-    this.setState({loading: true})
+  fetchMoreData = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=cda03860184d4e7989712e1e28ba1d8c&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+    this.setState({ page: this.state.page + 1 })
     let data = await fetch(url);
     let parseddata = await data.json();
     console.log(parseddata);
     this.setState({
-      articles: this.state.articles.concat(parseddata.articles),
-      loading: false
-     })
+      articles: this.state.articles.concat(parseddata.articles)
+      
+    })
   }
 
 
@@ -110,27 +109,27 @@ export default class News extends Component {
 
 
       <div >
-        <div className='container text-center'>
+        <div className='container text-center' style={{marginTop: '70px'}}>
           <h1> Today's Top Headlines</h1>
         </div>
 
         <InfiniteScroll
           dataLength={this.state.articles.length}
           next={this.fetchMoreData}
-          hasMore={this.state.articles.length !== this.state.totalResults}
-          loader={<h4>Loading...</h4>}
+          hasMore={(this.state.articles.length) !== (this.state.totalResults)}
+          // loader={<Spinner />}
         >
-        {this.setState.loading && <Spinner/>}
-        <div className='row mx-2'>
-          {this.state.articles.map((element) => {
-            return <div className='col-md-4' key={element.url} >
-              <NewsItem title={element.title} description={element.description} imageURL={element.urlToImage} newsURL={element.url} />
-            </div>
-          })}
-        </div>
-      </InfiniteScroll>
-                 
-          {/* <div className='d-flex justify-content-between'>
+          {this.setState.loading && <Spinner />}
+          <div className='row mx-2'>
+            {this.state.articles.map((element) => {
+              return <div className='col-md-4' key={element.url} >
+                <NewsItem title={element.title} description={element.description} imageURL={element.urlToImage} newsURL={element.url} />
+              </div>
+            })}
+          </div>
+        </InfiniteScroll>
+
+        {/* <div className='d-flex justify-content-between'>
             <button disabled={this.state.page<=1} type="button" onClick={this.handlePrevClick} className="btn btn-dark">&larr;Previous</button>
             <button type="button" onClick={this.handleNextClick} className="btn btn-dark">Next&rarr;</button>
           </div> */}
--- src/component/NewsItem.js
+++ src/component/NewsItem.js
@@ -15,7 +15,7 @@ export class NewsItem extends Component {
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
             <div className='text-center'>
-            <a href={newsURL} target="_blank" className="btn btn-sm btn-primary">Read More</a>
+            <a href={newsURL} rel="noreferrer" target="_blank" className="btn btn-sm btn-primary">Read More</a>
             </div>
           </div>
         </div>
